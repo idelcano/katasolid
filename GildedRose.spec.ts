c271536b60f7487d7e7cfd58760663d8ce241bd3
idelcano <ignaciodelcano@gmail.com>
2023-09-12 13:25:21 +0200

    start kata

---
+++ GildedRose.spec.ts
@@ -0,0 +1,114 @@
+import { GildedRose, Item, ItemInMemoryProvider } from "../GildedRose";
+
+describe("GildedRose Suite", () => {
+    it("Sell in value should be decreased", () => {
+        const itemProvider = new ItemInMemoryProvider([new Item("whatever", 10, 0)]);
+
+        const gildedRose = new GildedRose(itemProvider);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].sellIn).toBe(9);
+    });
+
+    it("Quality value should be decreased", () => {
+        const itemProvider = new ItemInMemoryProvider([new Item("whatever", 1, 10)]);
+
+        const gildedRose = new GildedRose(itemProvider);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toBe(9);
+    });
+
+    it("Quality value decreases twice as much when sell by is passed", () => {
+        const itemProvider = new ItemInMemoryProvider([new Item("whatever", 0, 10)]);
+
+        const gildedRose = new GildedRose(itemProvider);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toBe(8);
+    });
+
+    it("Quality is never negative", () => {
+        const itemProvider = new ItemInMemoryProvider([new Item("whatever", 0, 0)]);
+
+        const gildedRose = new GildedRose(itemProvider);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toBe(0);
+    });
+
+    it("Aged brie increases quality with age", () => {
+        const itemProvider = new ItemInMemoryProvider([new Item("Aged Brie", 0, 0)]);
+
+        const gildedRose = new GildedRose(itemProvider);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toBe(2);
+    });
+
+    it("Qualify never increases past fifty", () => {
+        const itemProvider = new ItemInMemoryProvider([new Item("Aged Brie", 5, 50)]);
+
+        const gildedRose = new GildedRose(itemProvider);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toBe(50);
+    });
+
+    it("Sulfuras never changes", () => {
+        const itemProvider = new ItemInMemoryProvider([new Item("Sulfuras, Hand of Ragnaros", 0, 80)]);
+
+        const gildedRose = new GildedRose(itemProvider);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toBe(80);
+        expect(items[0].sellIn).toBe(0);
+    });
+
+    it("Backstage pass increases quality by one if sell by greater than ten", () => {
+        const itemProvider = new ItemInMemoryProvider([new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20)]);
+
+        const gildedRose = new GildedRose(itemProvider);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toBe(21);
+    });
+
+    it("Backstage pass increases quality by two if sell by smaller than ten", () => {
+        const itemProvider = new ItemInMemoryProvider([new Item("Backstage passes to a TAFKAL80ETC concert", 6, 20)]);
+
+        const gildedRose = new GildedRose(itemProvider);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toBe(22);
+    });
+
+    it("Backstage pass increases quality by three if sell by smaller than five", () => {
+        const itemProvider = new ItemInMemoryProvider([new Item("Backstage passes to a TAFKAL80ETC concert", 4, 20)]);
+
+        const gildedRose = new GildedRose(itemProvider);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toBe(23);
+    });
+
+    it("Backstage pass loses value after sell by passes", () => {
+        const itemProvider = new ItemInMemoryProvider([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]);
+
+        const gildedRose = new GildedRose(itemProvider);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toBe(0);
+    });
+});
---
+++ GildedRoseKata.ts
@@ -0,0 +1,131 @@
+/*
+Hi and welcome to modified Gilded Rose kata version. 
+
+The goal is practice SOLID Principles refactoring this code.
+
+As you know, we are a small inn with a prime location in a prominent city ran by a friendly innkeeper named Allison.
+We also buy and sell only the finest goods. Unfortunately, our goods are constantly degrading in quality as they approach their sell by date.
+We have a system in place that updates our inventory for us. It was developed by a no-nonsense type named Leeroy, who has moved on to new adventures. 
+Your task is to add the new feature to our system so that we can begin selling a new category of items. First an introduction to our system:
+
+All items have a SellIn value which denotes the number of days we have to sell the item
+All items have a Quality value which denotes how valuable the item is
+At the end of each day our system lowers both values for every item
+Pretty simple, right? Well this is where it gets interesting:
+
+Once the sell by date has passed, Quality degrades twice as fast
+The Quality of an item is never negative
+"Aged Brie" actually increases in Quality the older it gets
+The Quality of an item is never more than 50
+"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+"Backstage passes", like aged brie, increases in Quality as it's SellIn value approaches; 
+   Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
+
+   We have recently signed a supplier of conjured items. This requires an update to our system:
+
+"Conjured" items degrade in Quality twice as fast as normal items
+
+In the future we can have a provider retrieving data from dhis2. Refactor the code to minimal change would be necessary when we need to do the change
+
+Feel free to make any changes to the previewUpdateQuality method and add any new code as long as everything still works correctly. 
+However, do not alter the Item class or Items property as those belong to the goblin in the corner who will insta-rage and one-shot you as he doesn't believe in shared code ownership.
+
+Just for clarification, an item can never have its Quality increase above 50, however "Sulfuras" is a legendary item and as such its Quality is 80 and it never alters.
+*/
+export class Item {
+    name: string;
+    sellIn: number;
+    quality: number;
+
+    constructor(name: string, sellIn: number, quality: number) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    toString(): string {
+        return `Name: ${this.name} SellIn: ${this.sellIn} Quality: ${this.quality}`;
+    }
+}
+
+export class ItemInMemoryProvider {
+    constructor(private items: Item[]) {}
+    get(): Item[] {
+        return this.items;
+    }
+}
+
+export class GildedRose {
+    constructor(private itemProvider: ItemInMemoryProvider) {}
+
+    updateQuality(): Item[] {
+        const items = this.itemProvider.get();
+
+        items.forEach(item => {
+            if (item.name !== "Aged Brie" && item.name !== "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.quality > 0) {
+                    if (item.name !== "Sulfuras, Hand of Ragnaros") {
+                        item.quality = item.quality - 1;
+                    }
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                    if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+            if (item.name !== "Sulfuras, Hand of Ragnaros") {
+                item.sellIn = item.sellIn - 1;
+            }
+            if (item.sellIn < 0) {
+                if (item.name !== "Aged Brie") {
+                    if (item.name !== "Backstage passes to a TAFKAL80ETC concert") {
+                        if (item.quality > 0) {
+                            if (item.name !== "Sulfuras, Hand of Ragnaros") {
+                                item.quality = item.quality - 1;
+                            }
+                        }
+                    } else {
+                        item.quality = item.quality - item.quality;
+                    }
+                } else {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+        });
+
+        return items;
+    }
+}
+
+function main() {
+    const itemProvider = new ItemInMemoryProvider([
+        new Item("+5 Dexterity Vest", 10, 20),
+        new Item("Aged Brie", 2, 0),
+        new Item("Elixir of the Mongoose", 5, 7),
+        new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        new Item("Conjured Mana Cake", 3, 6),
+    ]);
+
+    const gridedRose = new GildedRose(itemProvider);
+
+    const items = gridedRose.updateQuality();
+
+    console.log(items.map(item => item.toString()).join("\n"));
+}
+
+main();
